951ca5be089baa14ed84164d03d594e493a2d9a7
ChaosAttractor <lostattractor@gmail.com>
2023-12-15 08:05:54 +0800

    hydra: add flake inputs to allowed-uris

--- configuration/hydra/features/hydra.nix
+++ configuration/hydra/features/hydra.nix
@@ -3,6 +3,23 @@ _:
 {
   # Allowed URIs
   nix.settings.allowed-uris = [
+    "github:nixos/"
+    "github:NixOS/"
+    "github:LostAttractor/"
+    "github:nix-community/"
+    "github:tpwrules/"
+    "github:ryantm/"
+    "github:ezKEa/"
+    "github:the-argus/"
+    "github:hercules-ci/"
+    "github:rafaelmardojai/"
+    "github:numtide/"
+    "github:edolstra/"
+    "github:lnl7/"
+    "github:oxalica/"
+    "github:ipetkov/"
+    "github:nix-systems/"
+    "github:cachix/"
     "https://github.com"
     "https://api.github.com"
     "https://git.sr.ht"
